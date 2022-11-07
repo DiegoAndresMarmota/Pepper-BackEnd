cb18deaa0cc39fc5d5f388500059cdd5d860dc06
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-07 17:47:35 -0300

    Se corrige el import os en settings para ...makemigrations

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,7 +46,7 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://127.0.0.1:3000', 
+    'http://127.0.0.1:3000',
 ]
 
 MIDDLEWARE = [
