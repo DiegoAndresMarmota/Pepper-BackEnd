10cb09dc119caa159b80e36710bb5ba6c0b80db0
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-07 18:26:31 -0300

    Retornar información completa del usuario, login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+from .serializers import UserSerializer, UserSerializerWithToken
 
-# Create your views here.
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializers = UserSerializerWithToken(self.user).data
+
+        for token, user in serializers.items():
+            data[token] = user
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
