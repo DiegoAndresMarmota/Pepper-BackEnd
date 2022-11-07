5ce2716bc0331338b4bc347f5f313897da45b64c
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-07 18:23:04 -0300

    Serialización mediante token a usuario

--- users/serializers.py
+++ users/serializers.py
@@ -2,9 +2,10 @@ from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     is_admin = serializers.SerializerMethodField(read_only=True)
-    
+
     class Meta:
         model = User
         fields = ['id', 'user_name', 'email',
@@ -12,3 +13,16 @@ class UserSerializer(serializers.ModelSerializer):
 
     def get_is_admin(self, obj):
         return obj.is_staff
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'user_name', 'first_name',
+                  'email', 'is_admin', 'token', 'bio', 'image']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
