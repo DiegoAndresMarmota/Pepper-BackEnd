c3c5d45f5993cc77d18f8fe66b99e519c49e04a6
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-07 21:29:09 -0300

    Creación del endpoint [GET] para filtrar los blogs

--- blogs/views.py
+++ blogs/views.py
@@ -6,3 +6,11 @@ from rest_framework import status
 from .serializers import BlogSerializer
 from .models import Blog, Comment
 from users.models import User
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getBlogs(request):
+    blog = Blog.objects.filter().order_by('-date')
+    serializer = BlogSerializer(blog, many=True)
+    return Response(serializer.data)
